1ea1a674a5f9a92381240602240fc8893a6a47f3
davidmoradi <david@wishpond.com>
2016-02-14 16:53:25 -0800

    show and remove friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
 
 	def search
 		@users = User.search(params[:search_param])
-
 		if @users
 			@users = current_user.except_current_user(@users)
 			render partial: 'friends/lookup'
@@ -22,13 +21,18 @@ class UsersController < ApplicationController
 
 	def add_friend
 		@friend = User.find(params[:friend])
-		current_user.friendships.build(friend_id: @friend_id)
+		current_user.friendships.build(friend_id: @friend.id)
 
 		if current_user.save
 			redirect_to my_friends_path, notice: "Friend was successfully added"
 		else
 			redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend"
 		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@user_stocks = @user.stocks
 	end
 
 end
